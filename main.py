d98e589c49d1cb947105ac674d5d3acc4d717fd2
markrobertvandam <markrobertvandam@gmail.com>
2021-01-24 15:27:26 +0100

    finished program and added params

--- main.py
+++ main.py
@@ -25,11 +25,18 @@ def main():
         y += 100 * [i]
     y = np.array(y)
 
-    clf = RandomForestClassifier(n_estimators=200)
-    clf.fit(train_x,y)
-    ##result = cross_val_score(clf, test_x, y, cv=10)
-    ##print(np.average(result))
+    clf = RandomForestClassifier(n_estimators=200, criterion = "gini", max_depth = None,
+                                 min_samples_split = 4, min_samples_leaf = 1, min_weight_fraction_leaf = 0,
+                                 max_features = "auto", max_leaf_nodes = None, min_impurity_decrease = 0.0,
+                                 bootstrap = False, oob_score = False, n_jobs = -1, random_state = 1,
+                                 verbose = 1, warm_start = False, class_weight = None, ccp_alpha = 0.0,
+                                 max_samples = None)
 
+    result = cross_val_score(clf, train_x, y, cv=10)
+    print("crossval: ",np.average(result))
+
+    #clf.fit(train_x, y)
+    #print("Final score: ", clf.score(test_x, y))
 
 if __name__ == "__main__":
     main()
